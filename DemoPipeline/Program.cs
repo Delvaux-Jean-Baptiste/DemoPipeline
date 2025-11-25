bff2d08cc8fe7d7f185f4b527b725c00caa5edaf
Jean-Baptiste Delvaux <jb.delvaux@stag.technofuturtic.education>
2025-11-25 10:44:53 +0100

    init commit

---
+++ DemoPipeline/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoPipeline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
